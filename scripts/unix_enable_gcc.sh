ce007bf25a783108de18904b1cbbfffdac99e08a
Davit Kalantaryan <davit.kalantaryan@desy.de>
2024-07-22 16:41:10 +0200

    added sourcing file scripts/unix_enable_gcc.sh to enable next GCC

---
+++ scripts/unix_enable_gcc.sh
@@ -0,0 +1,8 @@
+#
+# Author: D. Kalantaryan (davit.kalantaryan@desy.de)
+#
+
+local gccRootDir=${1}
+
+export PATH=${gccRootDir}/bin:${PATH}
+export LD_LIBRARY_PATH=${gccRootDir}/lib64${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
