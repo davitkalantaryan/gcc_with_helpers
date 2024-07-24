30590e120c7b14a526046e209b0aac4696435adf
Davit Kalantaryan <installaccount@localhost.localdomain>
2024-07-24 17:15:16 +0200

    added env variables to take correct includes during compilation

--- scripts/unix_enable_gcc.sh
+++ scripts/unix_enable_gcc.sh
@@ -6,6 +6,9 @@ makeMainJob (){
     local gccRootDir=${1}
     export PATH=${gccRootDir}/bin:${PATH}
     export LD_LIBRARY_PATH=${gccRootDir}/lib64${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
+    export C_INCLUDE_PATH=${gccRootDir}/include${C_INCLUDE_PATH:+:$C_INCLUDE_PATH}
+    export CPLUS_INCLUDE_PATH=${gccRootDir}/include${CPLUS_INCLUDE_PATH:+:$CPLUS_INCLUDE_PATH}
+    export LIBRARY_PATH=${gccRootDir}/lib64${LIBRARY_PATH:+:$LIBRARY_PATH}
 }
 
 makeMainJob ${1}
