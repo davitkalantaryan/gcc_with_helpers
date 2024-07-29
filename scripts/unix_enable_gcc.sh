ee1555d8c4311af923f1682fd55dd195a149af1c
Davit Kalantaryan <davit.kalantaryan@desy.de>
2024-07-29 10:02:38 +0200

    glibc cloning is also done during init

--- scripts/unix_enable_gcc.sh
+++ scripts/unix_enable_gcc.sh
@@ -2,13 +2,37 @@
 # Author: D. Kalantaryan (davit.kalantaryan@desy.de)
 #
 
-makeMainJob (){
+makeMainJob() {
     local gccRootDir=${1}
+
+    # Add GCC binary to the PATH
     export PATH=${gccRootDir}/bin:${PATH}
+
+    # Add GCC libraries to the LD_LIBRARY_PATH
     export LD_LIBRARY_PATH=${gccRootDir}/lib64${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
-    export C_INCLUDE_PATH=${gccRootDir}/include${C_INCLUDE_PATH:+:$C_INCLUDE_PATH}
-    export CPLUS_INCLUDE_PATH=${gccRootDir}/include${CPLUS_INCLUDE_PATH:+:$CPLUS_INCLUDE_PATH}
+
+    # Set up C++ include path
+    local cplus_include_path=$(find ${gccRootDir} -name "iostream" -type f | head -n 1)
+    if [ -n "$cplus_include_path" ]; then
+        cplus_include_path=$(dirname $(dirname "$cplus_include_path"))
+	export CPLUS_INCLUDE_PATH=${cplus_include_path}${CPLUS_INCLUDE_PATH:+:$CPLUS_INCLUDE_PATH}
+    else
+        echo "Error: iostream header not found in ${gccRootDir}. C++ include path not set."
+	        return 1
+    fi
+
+    # Set up C include path
+    local c_include_path=$(find ${gccRootDir} -name "stdlib.h" -type f | head -n 1)
+    if [ -n "$c_include_path" ]; then
+        c_include_path=$(dirname "$c_include_path")
+	export C_INCLUDE_PATH=${c_include_path}${C_INCLUDE_PATH:+:$C_INCLUDE_PATH}
+    else
+        echo "Warning: stdlib.h header not found in ${gccRootDir}. C include path not set."
+    fi
+
+    # Set up library path
     export LIBRARY_PATH=${gccRootDir}/lib64${LIBRARY_PATH:+:$LIBRARY_PATH}
 }
 
+# Call the function with the provided GCC root directory
 makeMainJob ${1}
--- scripts/unix_prepare_repo_once.sh
+++ scripts/unix_prepare_repo_once.sh
@@ -17,6 +17,11 @@ if [ ! -d "$EXTRAS_DIRECTORY" ]; then
 fi
 
 cd ${EXTRAS_DIRECTORY}
+
 if [ ! -d "gcc" ]; then
     git clone https://github.com/davitkalantaryan/fork-gcc.git gcc
+fi
+
+if [ ! -d "glibc" ]; then
+    git clone https://github.com/davitkalantaryan/fork-glibc.git glibc
 fi
